956e12f7f57a38ed382032d361ac22196d015170
Thomas Bächler <thomas@archlinux.org>
2007-10-21 11:45:43 +0200

    change install.sh to exit on failure with return code 1

--- install.sh
+++ install.sh
@@ -1,23 +1,23 @@
 #!/bin/sh
 
-install -d -m755 ${DESTDIR}/etc/{rc.d,conf.d}
+install -d -m755 ${DESTDIR}/etc/{rc.d,conf.d} || exit 1
 
 for i in inittab rc.conf; do
-  install -D -m644 $i ${DESTDIR}/etc/$i
+  install -D -m644 $i ${DESTDIR}/etc/$i || exit 1
 done
 for i in rc.local rc.local.shutdown rc.multi rc.shutdown rc.single rc.sysinit; do
-  install -D -m755 $i ${DESTDIR}/etc/$i
+  install -D -m755 $i ${DESTDIR}/etc/$i || exit 1
 done
 
-install -D -m644 functions ${DESTDIR}/etc/rc.d/functions
+install -D -m644 functions ${DESTDIR}/etc/rc.d/functions || exit 1
 for i in network netfs; do
-  install -D -m755 $i ${DESTDIR}/etc/rc.d/$i
+  install -D -m755 $i ${DESTDIR}/etc/rc.d/$i || exit 1
 done
 
-gcc $CFLAGS -o minilogd minilogd.c || return 1
-install -D -m755 minilogd ${DESTDIR}/sbin/minilogd
+gcc $CFLAGS -o minilogd minilogd.c || exit 1
+install -D -m755 minilogd ${DESTDIR}/sbin/minilogd || exit 1
 
-install -D -m755 netcfg ${DESTDIR}/usr/bin/netcfg
-install -D -m644 profile-template ${DESTDIR}/etc/network-profiles/template
+install -D -m755 netcfg ${DESTDIR}/usr/bin/netcfg || exit 1
+install -D -m644 profile-template ${DESTDIR}/etc/network-profiles/template || exit 1
 
-install -D -m755 makedevs ${DESTDIR}/sbin/makedevs
+install -D -m755 makedevs ${DESTDIR}/sbin/makedevs || exit 1
