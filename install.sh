c79828e29eae787048535fa67726c2f174f6b2c5
Aaron Griffin <aaronmgriffin@gmail.com>
2009-03-10 13:59:16 -0700

    Add the adjtime cron job to install.sh
    Signed-off-by: Aaron Griffin <aaronmgriffin@gmail.com>

--- install.sh
+++ install.sh
@@ -3,6 +3,7 @@
 install -d -m755 ${DESTDIR}/etc/rc.d || exit 1
 install -d -m755 ${DESTDIR}/etc/conf.d || exit 1
 install -d -m755 ${DESTDIR}/etc/rc.d/functions.d/ || exit 1
+install -d -m755 ${DESTDIR}/etc/cron.hourly/ || exit 1
 
 for i in inittab rc.conf; do
   install -D -m644 $i ${DESTDIR}/etc/$i || exit 1
@@ -10,6 +11,8 @@ done
 for i in rc.local rc.local.shutdown rc.multi rc.shutdown rc.single rc.sysinit; do
   install -D -m755 $i ${DESTDIR}/etc/$i || exit 1
 done
+
+install -D -m755 adjtime.cron ${DESTDIR}/etc/cron.hourly/adjtime
 
 install -D -m644 functions ${DESTDIR}/etc/rc.d/functions || exit 1
 for i in network netfs; do
