a1e44e2633596e7f12364ec3e33b4012332046bb
Dave Reisner <dreisner@archlinux.org>
2011-08-19 13:29:16 -0400

    rc.d: clarify error when [[ -x /etc/rc.d/$i ]] fails
    A script marked a-x will be reported as missing, when in fact it's only
    non-executable.
    
    Signed-off-by: Dave Reisner <dreisner@archlinux.org>

--- rc.d
+++ rc.d
@@ -69,7 +69,8 @@ case $1 in
 			if [[ -x "/etc/rc.d/$i" ]]; then
 				env -i "${ENV[@]}" "/etc/rc.d/$i" "$action"
 			else
-				printf "${C_OTHER}:: ${C_FAIL}Error: ${C_DONE}Daemon script $i does not exist.${C_CLEAR}\n"
+				printf "${C_OTHER}:: ${C_FAIL}Error: ${C_DONE}Daemon script \`%s' does not exist or is not executable.${C_CLEAR}\n" \
+					"$i"
 			fi
 			(( ret += !! $? ))  # clamp exit value to 0/1
 		done
